001eb1adccd310cec5836290d44ba3b2ca1933e1
Marco <marco@Marc-Oliviers-MacBook-Pro.local>
2013-11-23 10:45:22 +0100

    change from client to server for the CC3000

--- robot_wifi/robot_wifi.ino
+++ robot_wifi/robot_wifi.ino
@@ -33,18 +33,19 @@ uint32_t t;
 int resultLength;
 
 // WiFi network (change with your settings !)
-#define WLAN_SSID       "yourNetwork"        // cannot be longer than 32 characters!
-#define WLAN_PASS       "yourPassword"
+#define WLAN_SSID       "myNetwork"        // cannot be longer than 32 characters!
+#define WLAN_PASS       "myPassword"
 #define WLAN_SECURITY   WLAN_SEC_WPA2 // This can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+
+// What TCP port to listen on for connections.
+#define LISTEN_PORT           8888    
 
 // Create CC3000 instances
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                                          SPI_CLOCK_DIV2);                                
                                          
-// Local server IP, port, and repository (change with your settings !)
-uint32_t ip = cc3000.IP2U32(192,168,0,1);
-int port = 80;
-String repository = "/arduino-robot-nn/";
+// Create server
+Adafruit_CC3000_Server robotServer(LISTEN_PORT);
 
 void setup() {
    
@@ -57,69 +58,95 @@ void setup() {
     pinMode(i, OUTPUT);  //set pin 4,5,6,7 to output mode
   }
   
-  // Initialise the CC3000 module
+  /* Initialise the module */
+  Serial.println(F("\nInitializing..."));
   if (!cc3000.begin())
   {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
     while(1);
   }
-
-  // Connect to  WiFi network
-  cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
-  Serial.println("Connected to WiFi network!");
-    
-  // Check DHCP
-  Serial.print(F("Requesting address from DHCP server..."));
-  for(t=millis(); !cc3000.checkDHCP() && ((millis() - t) < dhcpTimeout); delay(1000));
-  if(cc3000.checkDHCP()) {
-    Serial.println(F("OK"));
-  } else {
-    Serial.println(F("failed"));
-    return;
+  
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
   }
+   
+  Serial.println(F("Connected!"));
+  
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }  
+
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+ 
+  // Start listening for connections
+  robotServer.begin();
+  
+  Serial.println(F("Listening for connections..."));
  
 }
 
 void loop() {
   
-  // Send request to get relay state
-  Adafruit_CC3000_Client www = cc3000.connectTCP(ip, port);
-  if (www.connected()) {
-    Serial.println(F("Connected !"));   
-    www.println("GET " + repository + "server.php HTTP/1.0");
-    www.fastrprintln(F("Connection: close"));
-    www.fastrprintln(F(""));
-  } else {
-    Serial.println(F("Connection failed"));    
-    return;
-  }
-
-  while (www.connected()) {
-    while (www.available()) {
-      char c = www.read();
-      result = result + c;
-      
-      // Delete HTTP headers
+  // Try to get a client which is connected.
+  Adafruit_CC3000_ClientRef client = robotServer.available();
+  if (client) {
+     boolean currentLineIsBlank = true;
+     // Check if there is data available to read.
+     while (client.available()) {
+     
+       char c = client.read();
+       result = result + c;
+       Serial.write(c);
+       
+       // Delete HTTP headers
       if(result.endsWith("Content-Type: text/html"))
       {
         result="";
       }
-    }
-  }
-  www.close();
+       
+       if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();          
+       }
+       if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+       
+     }
+     // give the web browser time to receive the data
+    delay(5);
+    // close the connection:
+    client.close();
+    Serial.println("client disconnected");
+    
+    // Format result and extract the variables
+    format_result(motorCommand,result);
  
- // Format result and extract the variables
- format_result(motorCommand,result);
- 
- // Print received values
- Serial.println("Motor 1 speed: " + String(motorCommand[0]) + " and direction: " + String(motorCommand[2]));
- Serial.println("Motor 2 speed: " + String(motorCommand[1]) + " and direction: " + String(motorCommand[3]));
+    // Print received values
+    Serial.println("Motor 1 speed: " + String(motorCommand[0]) + " and direction: " + String(motorCommand[2]));
+    Serial.println("Motor 2 speed: " + String(motorCommand[1]) + " and direction: " + String(motorCommand[3]));
 
- // Send motor commands
- send_motor_command(speed_motor1,direction_motor1,motorCommand[0],motorCommand[2]);
- send_motor_command(speed_motor2,direction_motor2,motorCommand[1],motorCommand[3]);
+    // Send motor commands
+    send_motor_command(speed_motor1,direction_motor1,motorCommand[0],motorCommand[2]);
+    send_motor_command(speed_motor2,direction_motor2,motorCommand[1],motorCommand[3]);
  
- // Reset result variable
- result = "";
+    // Reset result variable
+    result = "";
+  }
            
 }
 
@@ -164,4 +191,30 @@ void format_result(int* array, String result) {
    }
    while(commaPosition >=0);  
 } 
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the IP address and other connection details
+*/
+/**************************************************************************/
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
+}
 
--- script.js
+++ script.js
@@ -32,3 +32,12 @@ function turnRight(){
 		directionRight: "0"} );
 }
 
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
